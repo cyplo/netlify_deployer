25b0fb09bb72481f153fb2b27cf215ef1563defa
Cyryl Płotnicki <cyplo@cyplo.net>
2019-01-31 10:48:42 +0000

    use pbr for version management

--- setup.py
+++ setup.py
@@ -4,21 +4,13 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='netlify_deployer',
-    version='0.0.1',
-    author='Cyryl Płotnicki',
-    author_email='cyplo@cyplo.net',
-    description='Package to help with deploying Netlify',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/cyplo/netlify_deployer',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'requests',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+)
+
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
