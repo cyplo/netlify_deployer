28b764945d448518234249391327a48ec891fae7
Cyryl Płotnicki <cyplo@cyplo.net>
2020-10-25 00:32:37 +0100

    add main wrapper

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 let
+  pkgs = import <nixpkgs> {};
   mach-nix = import (builtins.fetchGit {
     url = "https://github.com/DavHau/mach-nix/";
     ref = "refs/tags/3.0.1";
   }) {};
 in
   mach-nix.mkPythonShell {
-    requirements = builtins.readFile ./requirements.txt;
+    requirements = builtins.readFile ./requirements.txt + ''
+    setuptools
+    pbr
+    '';
   }
